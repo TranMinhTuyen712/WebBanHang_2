bb2b1fc61c967a5aa91a330a6af3ba78f225a447
tranminhtuyen <tranminhtuyen0712@gmail.com>
2024-06-08 20:46:41 +0700

    Add project files.

---
+++ WebBanHang/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace WebBanHang.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required, StringLength(50)]
+        public String Name { get; set; }
+        [Range(1, 100)]
+        public int DisplayOrder { get; set; }
+    }
+}
